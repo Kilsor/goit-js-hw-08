db187a5425f5c07d6951d2224bae2b8eea6230d4
Kilsor <kilsors@gmail.com>
2023-07-03 21:54:06 +0300

    hw8.3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+//Ініціалізація плеєра у файлі скрипта:
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+//Використання методу on() для відстежування події timeupdate та збереження часу відтворення у локальне сховище:
+player.on(
+  'timeupdate',
+  throttle(function (event) {
+    const currentTime = event.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
+
+//Під час перезавантаження сторінки використовуйте метод setCurrentTime() для відновлення відтворення зі збереженої позиції:
+window.addEventListener('load', function () {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(parseFloat(currentTime));
+  }
+});
